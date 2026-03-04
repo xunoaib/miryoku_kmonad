ab97d82739edc20bca5bb4f406d70a6649f2eac5
xunoaib <13055788+xunoaib@users.noreply.github.com>
2026-03-04 17:19:40 -0500

    Parse layers into layout dataclass

--- src/build/generate_layout.py
+++ src/build/generate_layout.py
@@ -1,37 +1,52 @@
 import re
+from dataclasses import dataclass
 from pathlib import Path
 
 
-def parse_keys(g: str):
+@dataclass
+class Layout:
+    src: list[list[str]]
+    layers: dict[str, list[list[str]]]
+
+
+def parse_keys(g: str, delim: str) -> list[list[str]]:
     rows = []
     for line in g.split('\n'):
         if line.startswith('(def') or line == ')':
             continue
-        fields = line.strip().split(' ')
+        fields = line.strip().split(delim)
         rows.append(fields)
     return rows
 
 
-def main():
-    text = Path('miryoku_kmonad.kbd').read_text()
-
-    keys = {}
+def parse_layout(text: str):
+    src = None
+    layers = {}
 
     for g in re.findall(r'\n\(def.*?\n\)', text, re.DOTALL):
         g = g.strip()
-
-        # print(repr(g))
-        # print()
-
         if g.startswith('(defcfg'):
             pass
         elif g.startswith('(defsrc'):
-            keys = parse_keys(g)
-            __import__('pprint').pprint(keys)
+            src = parse_keys(g, '\t')
         elif g.startswith('(deflayer'):
-            pass
+            name = g.split('\n')[0].split(' ')[1]
+            layer = parse_keys(g, '\t')
+            layers[name] = layer
         else:
             raise ValueError('unknown pattern:', repr(g))
+
+    assert src
+    return Layout(src, layers)
+
+
+def main():
+    text = Path('miryoku_kmonad.kbd').read_text()
+    layout = parse_layout(text)
+
+    print('Src:', layout.src)
+    print()
+    print('Layers:', list(layout.layers.keys()))
 
 
 if __name__ == '__main__':
