136bbc3d8fa7df682170f2c32cc16ae33155a600
xunoaib <13055788+xunoaib@users.noreply.github.com>
2026-03-04 17:12:18 -0500

    Parse defsrc

--- src/build/generate_layout.py
+++ src/build/generate_layout.py
@@ -1,8 +1,38 @@
 import re
 from pathlib import Path
 
-text = Path('miryoku_kmonad.kbd').read_text()
 
-for g in re.findall(r'\n\(def.*?\n\)', text, re.DOTALL):
-    print(repr(g))
-    print()
+def parse_keys(g: str):
+    rows = []
+    for line in g.split('\n'):
+        if line.startswith('(def') or line == ')':
+            continue
+        fields = line.strip().split(' ')
+        rows.append(fields)
+    return rows
+
+
+def main():
+    text = Path('miryoku_kmonad.kbd').read_text()
+
+    keys = {}
+
+    for g in re.findall(r'\n\(def.*?\n\)', text, re.DOTALL):
+        g = g.strip()
+
+        # print(repr(g))
+        # print()
+
+        if g.startswith('(defcfg'):
+            pass
+        elif g.startswith('(defsrc'):
+            keys = parse_keys(g)
+            __import__('pprint').pprint(keys)
+        elif g.startswith('(deflayer'):
+            pass
+        else:
+            raise ValueError('unknown pattern:', repr(g))
+
+
+if __name__ == '__main__':
+    main()
