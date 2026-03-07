2876dda1150968c6d3cc286cb8016cfea54c674b
xunoaib <13055788+xunoaib@users.noreply.github.com>
2026-03-07 03:59:06 -0500

    Apply all miryoku layer mappings and render

--- src/build/generate_layout.py
+++ src/build/generate_layout.py
@@ -27,17 +27,17 @@ class Key:
 
     @override
     def __repr__(self):
-        return self.raw
+        return repr(self.raw)
 
 
 @dataclass
 class Layout:
-    src: list[str]
+    src: list[Key]
     positions: list[tuple[int, int]]
-    layers: dict[str, list[str]]
+    layers: dict[str, list[Key]]
 
 
-def parse_keys(g: str, delim: str):
+def parse_keys(g: str, delim: str) -> tuple[list[Key], list[tuple[int, int]]]:
     keys = []
     positions = []
     row = 0
@@ -45,7 +45,7 @@ def parse_keys(g: str, delim: str):
         if line.startswith('(def') or line == ')':
             continue
         fields = line.strip().split(delim)
-        keys += fields
+        keys += [Key(f) for f in fields]
         positions += [(row, c) for c in range(len(fields))]
         row += 1
     return keys, positions
@@ -89,15 +89,18 @@ def apply_miryoku(miryoku: Layout, layout: Layout):
     '''
 
     mkey_positions = {
-        s: (i, p)
+        s.get_tap_key(): (i, p)
         for i, (s, p) in enumerate(zip(miryoku.src, miryoku.positions))
     }
 
-    layers = {layer: ['❌'] * len(layout.positions) for layer in miryoku.layers}
+    layers = {
+        layer: [Key('') for _ in range(len(layout.positions))]
+        for layer in miryoku.layers
+    }
 
     for srcpos, key in zip(layout.positions, layout.src):
         lidx = layout.positions.index(srcpos)
-        if res := mkey_positions.get(key):
+        if res := mkey_positions.get(key.get_tap_key()):
             midx, _mpos = res
             for layer, mkeys in miryoku.layers.items():
                 layers[layer][lidx] = mkeys[midx]
@@ -108,16 +111,31 @@ def apply_miryoku(miryoku: Layout, layout: Layout):
         print()
         print(maps)
 
+    return Layout(layout.src, layout.positions, layers)
 
-def display(layout: Layout):
-    d = {pos: key for pos, key in zip(layout.positions, layout.src)}
+
+def display(positions: list[tuple[int, int]], *keys_list: list[Key]):
+
+    # fully copy the first layer (including empty/unmapping keys), for geometry
+    d = {pos: key for pos, key in zip(positions, keys_list[0])}
+
+    # fill in any fallback keys
+    for keys in keys_list[::-1]:
+        for pos, key in zip(positions, keys):
+            if key.raw:
+                d[pos] = key
 
     cur_row = 0
     for (r, c), key in sorted(d.items()):
         if r != cur_row:
             print()
             cur_row = r
-        print(f'{key.ljust(4)} ', end='')
+        s = (key.get_tap_key() or '_').ljust(6)
+        if key.is_expression:
+            s = f'\033[93m{s}\033[0m'
+        else:
+            s = f'\033[92m{s}\033[0m'
+        print(f'{s} ', end='')
 
     print()
 
@@ -129,16 +147,25 @@ def main():
     miryoku = parse_layout(miryoku_str)
     c302 = parse_layout(c302_str)
 
-    miryoku.src = list(
-        ''.join(
-            (
-                '1 2 3 4 5' + '7 8 9 0 -',
-                'q w e r t' + 'u i o p [',
-                'a s d f g' + "j k l ; '",
-                'c v b' + 'n m ,',
-            )
+    new_src = list(
+        map(
+            Key, ' '.join(
+                (
+                    '1 2 3 4 5 ' + '7 8 9 0 -',
+                    'q w e r t ' + 'u i o p [',
+                    'a s d f g ' + "j k l ; '",
+                    'c v b ' + 'n m ,',
+                )
+            ).split()
         )
     )
+
+    print(new_src)
+
+    assert len(new_src) == len(
+        miryoku.src
+    ), f'{len(new_src)} != {len(miryoku.src)}'
+    miryoku.src = new_src
 
     print('\033[93m==== Miryoku ====\033[0m\n')
     print('Src:', miryoku.src)
@@ -155,7 +182,16 @@ def main():
     # print('Layers:', list(c302.layers.keys()))
 
     print('-' * 30)
-    apply_miryoku(miryoku, c302)
+    final = apply_miryoku(miryoku, c302)
+
+    print()
+    # display(final.src, final.positions)
+    print('Original Miryoku:\n')
+    display(miryoku.positions, miryoku.layers['U_BASE'])
+    print()
+    print('Adapted Miryoku:\n')
+    # display(final.positions, final.layers['U_BASE'], final.src)
+    display(final.positions, final.layers['U_BASE'])
 
 
 if __name__ == '__main__':
